1b57ddbd0e01345caef30bf19f6618265b5f1fb0
YuriySolomin <Yuriy.solomin1978@gmail.com>
2015-07-22 14:50:37 +0300

    add for history

---
+++ src/main/java/education/gtsIssues/GTSInteraction.java
@@ -0,0 +1,13 @@
+package education.gtsIssues;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: solomin-y
+ * Date: 7/22/15
+ * Time: 1:32 PM
+
+ */
+public interface GTSInteraction {
+
+    public void activity();
+}
--- src/main/java/education/listIssues/ArraysIssues.java
+++ src/main/java/education/listIssues/ArraysIssues.java
@@ -31,7 +31,6 @@ public class ArraysIssues {
                     source[j] = k;
                 }
             }
-
         }
         return source;
     }
